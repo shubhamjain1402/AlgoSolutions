e406619e90a6a8380a6c0b1da5f69b956321bac1
Shubham Jain <168662207+shubhamjain1402@users.noreply.github.com>
2026-01-07 10:58:29 +0530

    Time: 3 ms (74.88%), Space: 27.3 MB (73.16%) - LeetHub

---
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        unordered_map <int,int> inmap;
+        for(int i=0;i<inorder.size();i++){
+            inmap[inorder[i]]=i;
+        }
+        TreeNode* root = buildTree(preorder,0,preorder.size()-1,inorder,0,inorder.size()-1,inmap);
+        return root;
+    }
+    TreeNode* buildTree(vector<int>& preorder,int preStart , int preEnd , vector<int>& inorder , int inStart , int inEnd ,unordered_map <int,int> &inmap){
+        if(preStart > preEnd || inStart > inEnd)    return NULL;
+        TreeNode* root=new TreeNode(preorder[preStart]);
+        int inRoot = inmap[root->val];
+        int numsLeft=inRoot-inStart;
+        root->left=buildTree(preorder ,preStart+1,preStart+numsLeft , inorder, inStart , inRoot-1,inmap);
+        root->right=buildTree(preorder , preStart+numsLeft+1, preEnd , inorder , inRoot+1, inEnd , inmap);
+        return root;
+    }
+};
