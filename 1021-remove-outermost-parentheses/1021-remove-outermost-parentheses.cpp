fc3e636e6103506919ab568f68cbcb88a39d581f
Shubham Jain <168662207+shubhamjain1402@users.noreply.github.com>
2026-01-25 04:09:01 +0530

    Time: 0 ms (100%), Space: 8.9 MB (80.73%) - LeetHub

--- 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
@@ -1,27 +1,25 @@
 class Solution {
 public:
     string removeOuterParentheses(string s) {
-        string temp="";
-        stack <char> st;
-        vector <string> str;
-        for(int i=0;i<s.size();i++){
-            if(s[i] == '('){
-                st.push(s[i]);
-                temp+="(";
-            }
-            else if(s[i] == ')'){
-                st.pop();
-                temp+=")";
-                if(st.empty()){
-                    str.push_back(temp);
-                    temp="";
-                }
+        // Initialize result string
+        string result= "";
+        int level=0;
+        for(char ch : s){
+             // If we encounter '(', increase the level
+            if(ch == '('){
+                // If we're inside a primitive, add '(' to result
+                if(level > 0)   result+=ch;
+                // Increase the nesting level for '('
+                level++;
+            }        
+            else{
+                // If we encounter ')', decrease the level
+                // Decrease the nesting level for ')'
+                level--;
+                // If we're inside a primitive, add ')' to result
+                if(level > 0)   result+=ch;
             }
         }
-        string ans="";
-        for(int i=0;i<str.size();i++){
-            ans+=str[i].substr(1,str[i].size()-2);
-        }
-        return ans;
+        return result;
     }
 };
