14d3ae4bf2c84de4155eaf1053ffdecc39d62d0d
Shubham Jain <168662207+shubhamjain1402@users.noreply.github.com>
2026-01-31 19:09:58 +0530

    Time: 3 ms (63.5%), Space: 12.7 MB (30.09%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    string intToRoman(int num) {
+        vector<pair<int, string>> mpp = {
+            {1000, "M"}, 
+            {900, "CM"}, 
+            {500, "D"}, 
+            {400, "CD"}, 
+            {100, "C"},
+            {90, "XC"}, 
+            {50, "L"},   
+            {40, "XL"}, 
+            {10, "X"},   
+            {9, "IX"},
+            {5, "V"},    
+            {4, "IV"},   
+            {1, "I"},
+        };
+        string res = "";
+        for (auto& [x, y] : mpp) {
+            while (num >= x) {
+                res += y;
+                num -= x;
+            }
+        }
+        return res;
+    }
+};
