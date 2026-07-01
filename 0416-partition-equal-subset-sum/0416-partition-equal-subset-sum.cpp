8ec5e87f62db1768816e15cf56045891e7a35574
Shubham Jain <168662207+shubhamjain1402@users.noreply.github.com>
2026-07-01 23:11:14 +0530

    Time: 119 ms (64.07%), Space: 13.5 MB (86%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -5,20 +5,23 @@ public:
         int n=arr.size();
         for(auto &it: arr) sum+=it;
         if(sum&1) return false;
-        vector <vector<bool>> dp(n,vector <bool>(sum/2 +1,0));
+        vector <bool> prev(sum/2+1);
+        vector <bool> curr(sum/2+1);
+        prev[0]=curr[0]=0;
         int target=sum/2;
-        for(int i=0;i<n;i++) dp[i][0]=1;
-        if(arr[0] <= target) dp[0][arr[0]]=1;
+        for(int i=0;i<n;i++) prev[0]=1;
+        if(arr[0] <= target) prev[arr[0]]=1;
         for(int i=1;i<n;i++){
             for(int k=1;k<=target;k++){
-                bool notpick=dp[i-1][k];
+                bool notpick=prev[k];
                 bool pick=false;
                 if(arr[i] <= k){
-                    pick=dp[i-1][k-arr[i]];
+                    pick=prev[k-arr[i]];
                 }
-                dp[i][k]=pick|notpick;
+                curr[k]=pick|notpick;
             }
+            prev=curr;
         }
-        return dp[n-1][target];
+        return prev[target];
     }
 };
