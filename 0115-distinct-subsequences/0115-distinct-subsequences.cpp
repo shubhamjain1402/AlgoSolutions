59d53eb1495c9725e7addd3489009747803cdea6
Shubham Jain <168662207+shubhamjain1402@users.noreply.github.com>
2026-07-06 12:30:28 +0530

    Time: 7 ms (96.05%), Space: 8.9 MB (91.02%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,18 +1,23 @@
 class Solution {
+    // int func(string&s,string&t,int i,int j){
+    //     if(j<0) return 1;
+    //     if(i<0) return 0;
+    //     if(s[i]==t[j])  return func(s,t,i-1,j-1)+func(s,t,i-1,j);
+    //     else    return func(s,t,i-1,j);
+    // }
 public:
     int numDistinct(string s, string t) {
         int n=s.size();
         int m=t.size();
-        vector <vector <double>> dp(n+1,vector <double>(m+1));
-        for(int i=0;i<=n;i++) dp[i][0]=1;
-        for(int j=1;j<=m;j++) dp[0][j]=0;
+        // return func(s,t,n-1,m-1);
+        vector <double> dp(m+1);
+        dp[0]=1;
         for(int i=1;i<=n;i++){
-            for(int j=1;j<=m;j++){
+            for(int j=m;j>=1;j--){
                 if(s[i-1] == t[j-1])
-                dp[i][j]=dp[i-1][j-1]+dp[i-1][j]; // for taking or not taking to further check in s;
-                else dp[i][j]=dp[i-1][j];
+                dp[j]=dp[j-1]+dp[j];
             }
         }
-        return (int)dp[n][m];
+        return (int)dp[m];
     }
 };
