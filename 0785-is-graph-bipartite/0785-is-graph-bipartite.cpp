bb9f244d9f7472a0990fffe8957ef40dca3190c6
Shubham Jain <168662207+shubhamjain1402@users.noreply.github.com>
2026-07-06 15:25:07 +0530

    Time: 0 ms (100%), Space: 17.6 MB (26.81%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -1,18 +1,18 @@
 class Solution {
-private:
-    bool check(int start,int V, vector <int> color ,vector<vector<int>>& graph){
+    bool check(int start,int V,vector <int> &color,vector <vector <int>> &graph){
         queue <int> q;
         q.push(start);
         color[start]=0;
         while(!q.empty()){
             int node=q.front();
             q.pop();
-            for(auto adjacentNode : graph[node]){
-                if(color[adjacentNode] == -1){
-                    color[adjacentNode]=!color[node];
-                    q.push(adjacentNode);
+            int clr=color[node];
+            for(auto &it:graph[node]){
+                if(color[it]==-1){
+                    color[it]=!clr;
+                    q.push(it);
                 }
-                else if(color[adjacentNode] == color[node])    return false;
+                else if(color[it] == clr)   return false;
             }
         }
         return true;
@@ -20,10 +20,10 @@ private:
 public:
     bool isBipartite(vector<vector<int>>& graph) {
         int V=graph.size();
-        vector<int> color(V, -1);
+        vector <int> color(V,-1);
         for(int i=0;i<V;i++){
-            if(color[i] == -1){
-                if(check(i,V,color,graph) == false)   return false;
+            if(color[i]==-1){
+                if(check(i,V,color,graph)==false)   return false;
             }
         }
         return true;
