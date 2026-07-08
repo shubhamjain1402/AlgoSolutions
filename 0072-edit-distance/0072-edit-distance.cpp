3bb3348d0af93e316bd00ffcc0c1bab16a5dbf79
Shubham Jain <168662207+shubhamjain1402@users.noreply.github.com>
2026-07-08 15:28:27 +0530

    Time: 9 ms (38.31%), Space: 13.2 MB (41.48%) - LeetHub

---
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        int n=word1.size();
+        int m=word2.size();
+        vector <vector <int>> dp(n+1,vector <int>(m+1,0));
+        for(int j=0;j<=m;j++) dp[0][j]=j;
+        for(int i=0;i<=n;i++) dp[i][0]=i;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(word1[i-1]==word2[j-1])    dp[i][j]=0+dp[i-1][j-1];
+                else{
+                    dp[i][j]=min(
+                        1+dp[i][j-1],   //insert
+                        min(1+dp[i-1][j],  //delete
+                        1+dp[i-1][j-1]  //replace
+                    ));
+                }
+            }
+        }
+        return dp[n][m];
+    }
+};
