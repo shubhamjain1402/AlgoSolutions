52db9afe69ff674ed3419bfca518a9a9bbfc1e39
Shubham Jain <168662207+shubhamjain1402@users.noreply.github.com>
2026-07-11 13:40:21 +0530

    Time: 4 ms (72.79%), Space: 17.7 MB (61.1%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,21 +1,21 @@
 class Solution {
-    int func(int ind,int transaction,vector <vector<int>> &dp,vector <int>&prices,int n,int k){
-        if(ind == n)    return 0;
-        if(transaction == k)    return 0;
-        if(dp[ind][transaction] != -1)  return dp[ind][transaction];
-        if(transaction % 2 == 0){
-            return dp[ind][transaction]=max(-prices[ind]+ func(ind+1,transaction+1,dp,prices,n,k),
-            0+func(ind+1,transaction,dp,prices,n,k));
-        }
-        else{
-            return dp[ind][transaction]=max(prices[ind]+ func(ind+1,transaction+1,dp,prices,n,k),
-            0+func(ind+1,transaction,dp,prices,n,k));
-        }
-    }
 public:
     int maxProfit(int k, vector<int>& prices) {
-        int n=prices.size();
-        vector <vector <int>> dp(n,vector <int>(k*2,-1));
-        return func(0,0,dp,prices,n,k*2);
+        int n = prices.size();
+        vector<vector<int>> dp(n + 1, vector<int>(k * 2 + 1, 0));
+        for (int ind = n - 1; ind >= 0; ind--) {
+            for (int transaction = 0; transaction < k * 2; transaction++) {
+                if (transaction % 2 == 0) {
+                    dp[ind][transaction] =
+                        max(-prices[ind] + dp[ind + 1][transaction + 1],
+                            0 + dp[ind + 1][transaction]);
+                } else {
+                    dp[ind][transaction] =
+                        max(prices[ind] + dp[ind + 1][transaction + 1],
+                            0 + dp[ind + 1][transaction]);
+                }
+            }
+        }
+        return dp[0][0];
     }
 };
