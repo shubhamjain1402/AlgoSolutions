6cdef75674af0ee4f4b258bdce6820aea03ba682
Shubham Jain <168662207+shubhamjain1402@users.noreply.github.com>
2026-07-15 15:49:03 +0530

    Time: 19 ms (80%), Space: 14.1 MB (7.74%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
@@ -1,17 +1,4 @@
 class Solution {
-    // int func(int i, int j, vector<int>& cuts, vector<vector<int>>& dp) {
-    //     if (i > j)
-    //         return 0;
-    //     if (dp[i][j] != -1)
-    //         return dp[i][j];
-    //     int mini = INT_MAX;
-    //     for (int ind = i; ind <= j; ind++) {
-    //         int cost = cuts[j + 1] - cuts[i - 1] + func(i, ind - 1, cuts, dp) +
-    //                    func(ind + 1, j, cuts, dp);
-    //         mini = min(mini, cost);
-    //     }
-    //     return dp[i][j] = mini;
-    // }
 
 public:
     int minCost(int n, vector<int>& cuts) {
