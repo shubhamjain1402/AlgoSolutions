79daf9670e71c8f82f46aa50b82e89dbcf268a9e
Shubham Jain <168662207+shubhamjain1402@users.noreply.github.com>
2026-07-16 12:03:50 +0530

    Time: 6 ms (57.45%), Space: 12.6 MB (10.2%) - LeetHub

--- 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
@@ -1,24 +1,20 @@
 class Solution {
-    long long func(int ind, vector<int> &arr, vector<long long> &dp, int n, int k) {
-        if (ind == n) return 0;
-        if (dp[ind] != -1) return dp[ind];
-
-        long long maxAns = 0;
-        int maxi = INT_MIN;
-        long long len = 0;
-        for (int j = ind; j < min(n, ind + k); j++) {
-            len++;                          // track current partition length
-            maxi = max(maxi, arr[j]);       // track max element in this partition
-            long long sum = len * 1LL * maxi + func(j + 1, arr, dp, n, k);
-            maxAns = max(maxAns, sum);
-        }
-        return dp[ind] = maxAns;
-    }
-
 public:
     int maxSumAfterPartitioning(vector<int>& arr, int k) {
         int n = arr.size();
-        vector<long long> dp(n, -1);   // memoization array
-        return (int)func(0, arr, dp, n, k);
+        vector<long long> dp(n+1, 0);   // memoization array
+        for(int ind=n-1;ind>=0;ind--){
+            long long maxAns = 0;
+            int maxi = INT_MIN;
+            long long len = 0;
+            for (int j = ind; j < min(n, ind + k); j++) {
+                len++;                          // track current partition length
+                maxi = max(maxi, arr[j]);       // track max element in this partition
+                long long sum = len * 1LL * maxi + dp[j + 1];
+                maxAns = max(maxAns, sum);
+            }
+            dp[ind] = maxAns;
+        }
+        return (int)dp[0];
     }
 };
