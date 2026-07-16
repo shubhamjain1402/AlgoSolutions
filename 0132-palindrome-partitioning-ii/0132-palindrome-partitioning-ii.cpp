af3becc1a5c7da84d0dda2d54bd8cfc48f3fe487
Shubham Jain <168662207+shubhamjain1402@users.noreply.github.com>
2026-07-16 11:32:32 +0530

    Time: 1305 ms (47.23%), Space: 30.6 MB (20.68%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -1,30 +1,31 @@
 class Solution {
-    bool isPal(string &str){
-        int i=0;
-        int j=str.size()-1;
-        while(i<j){
-            if(str[i] != str[j])    return false;
-            i++,j--;
+    bool isPal(string& str) {
+        int i = 0;
+        int j = str.size() - 1;
+        while (i < j) {
+            if (str[i] != str[j])
+                return false;
+            i++, j--;
         }
         return true;
     }
-    int func(int i,int n,string &s,vector <int> &dp){
-        if(i == n)  return 0;
-        if(dp[i] != -1) return dp[i];
-        string temp="";
-        int mincost=INT_MAX;
-        for(int j=i;j<=n;j++){
-            temp+=s[j];
-            if(isPal(temp)){
-            int cost=1+func(j+1,n,s,dp);
-            mincost=min(mincost,cost);}
-        }
-        return dp[i]=mincost;
-    }
+
 public:
     int minCut(string s) {
-        int n=s.size();
-        vector <int> dp(n,-1);
-        return func(0,n,s,dp)-1;
+        int n = s.size();
+        vector<int> dp(n + 1, 0);
+        for (int i = n - 1; i >= 0; i--) {
+            string temp = "";
+            int mincost = INT_MAX;
+            for (int j = i; j <= n - 1; j++) {
+                temp += s[j];
+                if (isPal(temp)) {
+                    int cost = 1 + dp[j + 1];
+                    mincost = min(mincost, cost);
+                }
+            }
+            dp[i] = mincost;
+        }
+        return dp[0] - 1;
     }
 };
