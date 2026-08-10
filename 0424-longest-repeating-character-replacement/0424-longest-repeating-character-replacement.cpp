105411dce244813449e03b050c28dce59d3847ef
Shubham Jain <168662207+shubhamjain1402@users.noreply.github.com>
2026-08-11 01:23:08 +0530

    Time: 0 ms (100%), Space: 10.8 MB (93.83%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,17 +1,20 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        int left=0,right=0,maxlen=0,maxf=0;
-        vector <int> hash(26,0);
-        while(right < s.size()){
-            hash[s[right]-'A']++;
-            maxf=max(maxf,hash[s[right]-'A']);
-            if((right - left + 1)-maxf > k){
-                hash[s[left]-'A']--;
+        int left=0;
+        vector <int> freq(26,0);
+        int maxfreq=0;
+        int maxlen=0;
+        for(int right= 0 ; right < s.size() ; right++){
+            freq[s[right]-'A']++;
+            maxfreq=max(maxfreq,freq[s[right]-'A']);
+            if(right-left+1 - maxfreq > k){
+                freq[s[left] - 'A']--;
                 left++;
             }
-               maxlen = max(maxlen, right - left + 1);
-                right++;
+            if(right-left+1 - maxfreq <= k){
+                maxlen=max(maxlen,right-left+1);
+            }
         }
         return maxlen;
     }
