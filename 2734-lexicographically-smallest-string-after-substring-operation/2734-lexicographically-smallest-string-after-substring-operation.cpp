1b9a15a6b66c4d994ef9fd14eb6bcc57d286f352
Shubham Jain <168662207+shubhamjain1402@users.noreply.github.com>
2026-09-03 03:05:26 +0530

    Time: 7 ms (39.49%), Space: 43.9 MB (88.85%) - LeetHub

--- 2734-lexicographically-smallest-string-after-substring-operation/2734-lexicographically-smallest-string-after-substring-operation.cpp
+++ 2734-lexicographically-smallest-string-after-substring-operation/2734-lexicographically-smallest-string-after-substring-operation.cpp
@@ -1,29 +1,10 @@
 class Solution {
 public:
     string smallestString(string s) {
-        int flag=0;
-        string res="";
-        int i=0;
-        while(i<s.size()){
-            if(s[i] != 'a'){
-                res+=(char)(s[i]-1);
-                flag=1;
-                i++;
-            }
-            else if(s[i] == 'a' && flag==0){
-                res+=s[i];
-                i++;
-            }
-            else if(s[i] == 'a' && flag == 1) {
-                break;
-            }
-        }
-        int n=s.length();
-        if(flag == 0){
-            res[n-1]='z';
-            return res;
-        }
-        while(flag != 0 && i<s.size()){res+=s[i];i++;}
-        return res;
+        int i = 0, n = s.size();
+        while (i < n && s[i] == 'a') i++;
+        if (i == n) s[n - 1] = 'z';
+        while (i < n && s[i] != 'a') --s[i++];
+        return s;
     }
 };
